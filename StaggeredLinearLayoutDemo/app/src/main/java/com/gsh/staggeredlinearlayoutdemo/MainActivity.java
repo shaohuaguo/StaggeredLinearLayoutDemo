80283c394d4d8a1066bdfdd5983dbcd3214e54f6
damon535 <458062163@qq.com>
2016-09-02 14:41:01 +0800

    添加modified

--- StaggeredLinearLayoutDemo/app/src/main/java/com/gsh/staggeredlinearlayoutdemo/MainActivity.java
+++ StaggeredLinearLayoutDemo/app/src/main/java/com/gsh/staggeredlinearlayoutdemo/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StaggeredLinearLayout staggeredLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,41 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        initLayout();
+        initData();
     }
+
+    private void initData() {
+        float horizontalSpace = getResources().getDimension(R.dimen.pregnancy_wall_text_horizontal_space);
+        staggeredLinearLayout.setTextHorizontalSpace((int) horizontalSpace);
+        float verticalSpace = getResources().getDimension(R.dimen.pregnancy_wall_text_vertical_space);
+        staggeredLinearLayout.setTextVerticalSpcae((int) verticalSpace);
+        float textSize = getResources().getDimension(R.dimen.pregnancy_wall_text_size);
+        staggeredLinearLayout.setTextSize((int) textSize);
+        float textPaddingLeftAndRight = getResources().getDimension(R.dimen.pregnancy_wall_text_padding_left_or_right);
+        staggeredLinearLayout.setTextPadding((int) textPaddingLeftAndRight, 0, (int) textPaddingLeftAndRight, 0);
+        staggeredLinearLayout.setTextColor(getResources().getColor(R.color.color_f9));
+        float textHeight = getResources().getDimension(R.dimen.pregnancy_wall_text_height);
+        staggeredLinearLayout.setTextHeight((int) textHeight);
+        staggeredLinearLayout.setChildList(getCacheLabels());
+    }
+
+
+    private List<StaggeredLinearLayout.ThinkLabelsEntity> getCacheLabels() {
+        List<StaggeredLinearLayout.ThinkLabelsEntity> list = new ArrayList<>();
+        String[] content = new String[]{"天生傲骨°怎能服输", "强求不来你不如放弃", "你的嘴欠缝", "笨小孩", "楼下贞子的妹妹", "蓦然回首你咋还没走", "今天你被整了吗", "宿命的偏执っ 抒写了谁的离歌", "不二小姐", "王妃范儿", "将心比心。OK？", "你要明白 自个心自个疼"};
+        for (int i = 0; i < content.length; i++) {
+            int id = (int) (Math.random() * Integer.MAX_VALUE);
+            StaggeredLinearLayout.ThinkLabelsEntity thinklabel = new StaggeredLinearLayout.ThinkLabelsEntity(content[i], id);
+            list.add(thinklabel);
+        }
+        return list;
+    }
+
+    private void initLayout() {
+        staggeredLinearLayout = (StaggeredLinearLayout) findViewById(R.id.staggered_linear_layout);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
